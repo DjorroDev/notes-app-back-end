dd985f40a66b9341f9329f23f1a63515ab3c557a
Djorro <DjorroDev@users.noreply.github.com>
2023-05-13 20:04:23 +0700

    intial commit

---
+++ src/notes.js
@@ -0,0 +1,20 @@
+const notes = [
+  {
+    title: 'Ikan',
+    tags: ['Bapakkau', 'asdsa', 'asd'],
+    body: 'lorem ipsum dolor sit amet',
+    id: '1-IFu0gTWMc4WbZv',
+    createdAt: '2023-03-26T13:53:22.989Z',
+    updatedAt: '2023-03-26T13:53:22.989Z',
+  },
+  {
+    title: 'Pante',
+    tags: ['Bapakkau', 'sddd', 'asd'],
+    body: 'lorem ipsusadasdasdasddm dolor sit amet',
+    id: '1-IFu0gTWMc4Wbasdzv',
+    createdAt: '2023-03-26T13:53:22.989Z',
+    updatedAt: '2023-03-26T13:53:22.989Z',
+  },
+];
+
+module.exports = notes;
